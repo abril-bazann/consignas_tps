d529a06d259156a6020006c8b1341d1e2c5543c7
abril <abrilbazan28@gmail.com>
2025-01-10 20:57:18 -0300

    resuelvo segunda consigna de ejes cartesianos

--- tp1/consignas.cpp
+++ tp1/consignas.cpp
@@ -1,3 +1,4 @@
+//RESPUESTAS A CONSIGNAS DEL TP2 DE INFORMÁTICA
 #include <iostream>
 using namespace std;
 
@@ -11,5 +12,29 @@ cin>>numero;
 if(numero<0){
     cout<<"El valor es negativo";
 }else{cout<<"El numero es positivo";};
-
 }
+
+//---------------------------------------------------------------------------
+/*Complete el siguiente programa de C++ para que solicite se ingrese por teclado las 
+coordenadas x0 e y0 de un punto en R2. Si el punto está en el primer cuadrante, 
+mostrar por pantalla el mensaje: "El punto pertenece al primer cuadrante". 
+Se debe considerar que el punto está en el primer cuadrante cuando se cumple que x0 es mayor que 
+cero y que y0 es mayor o igual a cero..*/
+int x0,y0;
+cout<<"Igrese la coordenada x0 de R2: ";
+cin>>x0;
+cout<<endl;
+cout<<"Igrese la coordenada y0 de R2: ";
+cin>>y0;
+//primer cuadrante ++ , segundo cuadrante -+ , tercer cuadrante -- ,  cuarto cuadrante +-
+
+if(x0>0 && y0>0){
+    cout<<"Esa coordenada se encuentra dentro del primer cuadrante";
+    }else if(x0<0 && y0>0){
+        cout<<"Esa coordenada se encuentra en el segundo cuadrante";
+        }else if(x0<0 && y0<0){
+            cout<<"Esa coordenada está en el tercer cuadrante";
+            }else if(x0>0 && y0<0){
+                cout<<"Esa coordenada se encuentra en el cuarto cuadrante";
+                }
+
